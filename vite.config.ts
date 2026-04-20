df5958df6bb07bab9a8929efdf31004556283bd7
JustKeepSilence <hekang0426@gmail.com>
2026-04-20 15:36:13 +0800

    fix:remove pdfobject remote script in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import manifest from './src/manifest.json';
 export default defineConfig({
   plugins: [
     crx({ manifest }),
+    {
+      name: 'strip-cdn-plugin',
+      enforce: 'post', 
+      renderChunk(code) {
+        if (code.includes('cdnjs.cloudflare.com')) {
+          const cleanCode = code.replace(
+            /https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/pdfobject\/[\d\.]+\/pdfobject\.min\.js/gi,
+            '' 
+          );
+          return { code: cleanCode, map: null };
+        }
+        return null;
+      }
+    }
   ],
   server: {
     port: 5173,
